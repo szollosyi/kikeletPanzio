9d59ff6c2e1f91e0704bffb94b9a512c61c6f701
szollosyistvan <szollosyi@kkszki.hu>
2024-05-21 08:26:56 +0200

    Osztály hozzáadása

---
+++ kikeletPanzio/Szoba.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kikeletPanzio
+{
+    internal class Szoba
+    {
+        int szobaszam;
+        int ferohely;
+        int ar;
+
+        public Szoba(int szobaszam, int ferohely, int ar)
+        {
+            this.Szobaszam = szobaszam;
+            this.Ferohely = ferohely;
+            this.Ar = ar;
+        }
+
+        public int Szobaszam { get => szobaszam; set => szobaszam = value; }
+        public int Ferohely { get => ferohely; set => ferohely = value; }
+        public int Ar { get => ar; set => ar = value; }
+
+        public override string ToString()
+        {
+            return $"Szobaszám: {szobaszam}\nFérőhely: {ferohely}\nÁr: {ar}";
+        }
+    }
+}
